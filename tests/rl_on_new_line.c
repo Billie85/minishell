761c3d210f694df8ed8fdbc3f4520601cab5ee76
Angelinne <kayumi85@hotmail.com>
2022-08-24 06:52:41 +0900

    new

--- tests/rl_on_new_line.c
+++ tests/rl_on_new_line.c
@@ -1,23 +1,30 @@
 //tell the update routines that we have moved onto a new line
 //通常、改行を出力した後、新しい (空の) 行に移動したことを更新ルーチンに伝えます。
 
-#include <stdio.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <limits.h>
+# include <errno.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <signal.h>
 
-#include <readline/readline.h>
-#include <signal.h>
-
-int main(void) {
-	
-	if (signal == SIGINT)
-	{
-		ft_putstr_fd("\n", 2);
-		rl_on_new_line();
-		rl_replace_line("", 0);
-		rl_redisplay();
-	}
-	if (signal == SIGQUIT)
-	{
-		rl_on_new_line();
-		rl_redisplay();
-	}
+void sig_handler()
+{
+    printf ("\n");
+    rl_on_new_line();
+    rl_replace_line("", 0);
+    rl_redisplay();
 }
+int main()
+{
+    signal(SIGQUIT, SIG_IGN);
+    signal(SIGINT, sig_handler);
+    while (1)
+    {
+        char *str = readline(">");
+		printf("%s",str);
+    }
+} 
