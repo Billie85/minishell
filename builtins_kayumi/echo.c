30e06c41f308024444a38a69075e6930df218e00
Angelinne <kayumi85@hotmail.com>
2022-08-26 01:57:20 +0900

    echo.c

---
+++ builtins_kayumi/echo.c
@@ -0,0 +1,41 @@
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stddef.h>
+# include <ctype.h>
+
+void	ft_putchar_fd(char c, int fd)
+{
+	write (fd, &c, 1);
+}
+
+void	ft_putstr_fd(char *s, int fd)
+{
+	if (s == NULL)
+		return ;
+	while (*s)
+	{
+		write (fd, s, 1);
+		s++;
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	size_t	i;
+	size_t len;
+
+	i = 0;
+	while(argv[i])
+		len = i++;
+
+	i = 1;
+	while(i < len + 1)
+	{
+		ft_putstr_fd(argv[i], 1);
+		ft_putchar_fd(' ', 1);
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
