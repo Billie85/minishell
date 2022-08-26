0c8491cad1685603e66f74b2e533e82e76c74528
Angelinne <kayumi85@hotmail.com>
2022-08-26 19:59:00 +0900

    finish echo -n

--- builtins_kayumi/echo_n.c
+++ builtins_kayumi/echo_n.c
@@ -5,7 +5,7 @@
 # include <ctype.h>
 
 //オプション付き -n -> 最後の改行を出力しない
-//まだ終わってない
+
 void	ft_putchar_fd(char c, int fd)
 {
 	write (fd, &c, 1);
@@ -22,21 +22,23 @@ void	ft_putstr_fd(char *s, int fd)
 	}
 }
 
-int main(int argc, char *argv[])
+int	main(int argc, char *argv[])
 {
 	size_t	i;
 	size_t len;
+	size_t delet_last_space;
 
 	i = 0;
 	while(argv[i])
 		len = i++;
+	delet_last_space = len -1;
 
 	i = 1;
 	while(i < len + 1)
 	{
 		ft_putstr_fd(argv[i], 1);
-		ft_putchar_fd(' ', 1);
-
+		if (i < len)
+			ft_putchar_fd(' ', 1);
 		i++;
 	}
 	return (0);
