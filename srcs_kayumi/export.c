0309e1f2a92d362ff5c087549fa77766ae07433e
Angelinne <kayumi85@hotmail.com>
2022-09-02 21:16:44 +0900

    update export.c

--- srcs_kayumi/export.c
+++ srcs_kayumi/export.c
@@ -1,11 +1,53 @@
 #include "minishell2.h"
 
 size_t	ft_strlen(const char *c);
+void	ft_putstr_fd(char *s, int fd);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+char	*ft_strdup(const char *s);
+void	ft_putchar_fd(char c, int fd);
+char	**ft_split(char const *str, char c);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+
+char	*strlcpy__(char *dst, const char *src, char equal)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while(src[i] != equal)
+	i++;
+	dst = (char *)malloc(sizeof(char) + i + 1);
+	j = 0;
+	while (src[j] != equal)
+	{	
+		dst[j] = src[j];
+		j++;
+	}
+	dst[j] = '\0';
+	return (dst);
+}
 
 int main(int argc, char **argv)
 {
-	size_t str;
+	char	*tmp;
+	int		i;
+	int		j;
+	char	*up_to_equal;
+	char	*copy_str;
+	size_t	len;
 
-	str = ft_strlen(argv[2]);//export->argv[1]
-	printf("%ld\n", str);
+	i = 1;
+	while(argv[i])
+	{
+		copy_str = ft_strdup(argv[2]);
+		up_to_equal = strlcpy__(up_to_equal, argv[i], '=');//　=の前の処理まで
+		j = 0;
+		while(copy_str[j] != '=')
+			j++;
+		len = ft_strlen(copy_str);
+		tmp = ft_substr(copy_str, j + 1, len -j -1);
+		i++;
+	}
+	printf("%s", tmp);
+	return (0);
 }
--- srcs_kayumi/minishell2.h
+++ srcs_kayumi/minishell2.h
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "libft/libft.h"
+#include <unistd.h>
 
 typedef struct s_envp
 {
