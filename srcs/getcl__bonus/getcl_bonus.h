20a3f613220bbbb03e87319d6e33c5102cc12672
Angelinne <kayumi85@hotmail.com>
2022-09-10 21:36:48 +0900

    new

---
+++ srcs/getcl__bonus/getcl_bonus.h
@@ -0,0 +1,9 @@
+#ifndef GETCL_BONUS_H
+# define GETCL_BONUS_H
+# include "../minishell_bonus.h"
+
+char	*gcl_std(char	*cl, size_t	B);
+char	*gcl_dq(char	*cl, size_t	B);
+char	*gcl_sq(char	*cl, size_t	B);
+char	*gcl_nc(char	*cl, size_t	B);
+#endif
